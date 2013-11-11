fcd848d20e33e791ab1f6e8edcfd1a173df18bbc
Isaac Hodes <ihodes@mac.com>
2013-11-11 03:57:11 -0500

    add ga

--- src/skrio/pages.clj
+++ src/skrio/pages.clj
@@ -17,6 +17,13 @@
       [:link {:href "/public/style.css" :rel "stylesheet"}]
       [:script {:type "text/javascript" :src "//use.typekit.net/rim7ixs.js"} "var noop;"]
       [:script {:type "text/javascript"} "try{Typekit.load();}catch(e){}"]
+      [:script {:type "text/javascript"} "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45615886-1', 'skr.io');
+  ga('send', 'pageview');"]
       ]
      
      [:body
